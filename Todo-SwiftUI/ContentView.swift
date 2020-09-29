aaa72e51fda8ee8959dedaf97f67abf478f50b36
safarsafarov <safarov.safar@hotmail.com>
2020-09-29 23:04:55 +0800

    first

---
+++ Todo-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Todo-SwiftUI
+//
+//  Created by Safar Safarov on 2020/9/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Todo-SwiftUI/Todo_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_SwiftUIApp.swift
+//  Todo-SwiftUI
+//
+//  Created by Safar Safarov on 2020/9/29.
+//
+
+import SwiftUI
+
+@main
+struct Todo_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
